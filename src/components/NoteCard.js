cc95c659d52101fa9c42f72e688c450c5627af8e
Kalen Hammann <kalen7@gmail.com>
2018-01-01 15:16:23 -0500

    Added NoteCard subcomponent and used it to list the notes in list.js

---
+++ src/components/NoteCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NoteCard extends React.component {
+    render() {
+        return (
+            <div className="note-card-container">
+              NoteCard component
+            </div>
+        );  
+    }
+}
+
+export default NoteCard;
